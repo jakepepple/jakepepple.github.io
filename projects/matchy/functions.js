2e34a66a2a23b4a13552440682af27aa6d211647
Jake Pepple <jakepeppledrums@gmail.com>
2017-06-14 00:35:54 +0000

    matchy complete

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -14,31 +14,57 @@
 //////////////////////////////////////////////////////////////////////
   function search(animals, name){
       for (var i = 0; i < animals.length; i++){
-          if (name === animals[i].name){
+          if (name.toUpperCase() === animals[i].name.toUpperCase()){
               return animals[i];
-          } else {
-              return null;
           }
-      }
+            
+          
+      } return null;
   }
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+  function replace(animals, name, replacement){
+     
+      for (var i = 0; i < animals.length; i++){
+          if (name.toUpperCase() === animals[i].name.toUpperCase()){
+              animals[i] = replacement;
+          }
+  }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+  function remove(animals, name){
+      
+      for (var i = 0; i < animals.length; i++){
+          if (name.toUpperCase() === animals[i].name.toUpperCase()){
+              animals.splice(i, 1);
+          }
+  }
+}
 
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Create ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+  function add(animals, animal){
+      var unique = true;
+      for (var i = 0; i < animals.length; i++){
+          if (animal.name === animals[i].name){
+              unique = false;
+          }
+      }
+      if (animal.name.length > 0 && animal.species.length > 0 && unique === true){
+          animals.push(animal);
+      }
+      
+  }
 
 
 /** 
